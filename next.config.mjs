ffb17368008ffeb76648037536acc41fb23ed943
NathanSaludes <saludesnathaniel@gmail.com>
2024-09-04 12:46:40 +0800

    Undo changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,10 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   webpack: (config) => {
-      config.resolve.alias["@"] = path.resolve(__dirname);
-      return config;
-   },
    images: {
       remotePatterns: [
          {
             protocol: "https",
             hostname: "**",
-         },
-         {
-            hostname: "gs://upria-bucket-test/",
          },
       ],
    },
